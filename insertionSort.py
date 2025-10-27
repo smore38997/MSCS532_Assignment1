a036f6c34257907a92e7cb540a510ce2da415db7
Shreyas <more3118@gmail.com>
2025-10-26 20:53:13 -0500

    Added The Insertion sort Alogrithm

--- insertionSort.py
+++ insertionSort.py
@@ -1,14 +1,19 @@
 
 
+# MSCS-532-B01
+# Assisgnment 1 
+# By Shreyas More 
 
 
-def insertionSort():
-    print("Created Function!")
+def insertionSort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] < key:
+            arr[j + 1] = arr[j]
+            j -= 1
 
+        arr[j + 1] = key
 
-
-
-
-
-
-insertionSort()
+    return arr
+    
